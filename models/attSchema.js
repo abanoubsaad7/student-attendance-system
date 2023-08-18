37352f4e6deb76655bb8d36f2bfa30ee466d396e
Abanoub Saad <88698601+abanoubsaad7@users.noreply.github.com>
2023-08-18 13:32:01 +0300

    Update attSchema.js

--- models/attSchema.js
+++ models/attSchema.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
+const moment = require("moment-timezone");
 const Schema = mongoose.Schema;
 
 const attendanceSchema = new Schema({
   userID: mongoose.Types.ObjectId,
-  date: { type: Date, default: Date.now()  }
+  date: { type: Date, default: moment().tz("Africa/Cairo").format("YYYY-MM-DD hh:mm A") }
 });
 
-const Attendance = mongoose.model("Attendance",attendanceSchema);
+const Attendance = mongoose.model("Attendance", attendanceSchema);
 
 module.exports = Attendance;
